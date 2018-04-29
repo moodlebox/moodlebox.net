ffc07e08d9e9f50dfb936b16f8937a29f3ed79bc
Nicolas Martignoni <nicolas@martignoni.net>
2018-04-29 19:09:43 +0200

    Update integration with gulp version 4.

--- gulpfile.js
+++ gulpfile.js
@@ -2,61 +2,51 @@ var gulp      = require('gulp'),
     concat    = require('gulp-concat'),
     minify    = require('gulp-clean-css'),
     uglify    = require('gulp-uglify'),
-    beautify  = require('gulp-html-prettify'),
-    trim      = require('gulp-remove-empty-lines'),
-    rename    = require('gulp-rename-plus'),
+    beautify  = require('gulp-jsbeautifier'),
+    replace   = require('gulp-replace'),
+    rename    = require('gulp-ext-replace'),
     gulpif    = require('gulp-if'),
     exec      = require('child_process').exec,
     del       = require('del');
 
-// gulp.task('default', [ 'reset', 'css', 'js', 'hugo', 'html', 'clean' ]);
-gulp.task('default', [ 'reset', 'css', 'hugo', 'html', 'clean' ]);
-
 gulp.task('reset', function(){
   return del([
     'public/**/*',
-    'themes/hugo-moodlebox-theme/static/css/**/*.min.css',
-//       'themes/hugo-moodlebox-theme/static/static/js/**/*',
-    ''
+    'themes/hugo-moodlebox-theme/static/css/**/*.min.css'
   ]);
 });
 
 gulp.task('css', function(){
   return gulp.src(['./static/css/**/*.css', './themes/hugo-moodlebox-theme/static/css/**/*.css'])
-//     .pipe(concat('main.css'))
-    .pipe(gulpif(file => !(file.path.includes('.min.css')), rename({suffix:'min.css'})))
+    .pipe(gulpif(file => !(file.path.includes('.min.css')), rename('min.css')))
     .pipe(minify())
     .pipe(gulp.dest('themes/hugo-moodlebox-theme/static/css'))
 });
 
 gulp.task('js', function(){
   return gulp.src(['./static/js/**/*.js', './themes/hugo-moodlebox-theme/static/js/**/*.js'])
-//     .pipe(gulpif(file => !(file.path.includes('.min.js')), rename({suffix:'min.js'})))
-//     .pipe(concat('main.js'))
-//     .pipe(uglify())
     .pipe(gulp.dest('themes/hugo-moodlebox-theme/static/js'))
 });
 
-// gulp.task('hugo', ['reset', 'css', 'js'], function (fetch) {
-gulp.task('hugo', ['reset', 'css'], function (fetch) {
+gulp.task('hugo', gulp.series('reset', 'css', 'js', function (fetch) {
   return exec('hugo', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     fetch(err);
   });
-})
+}));
 
-gulp.task('html', ['hugo'], function() {
+gulp.task('html', function() {
   return gulp.src(['public/**/*.html', 'public/**/*.xml'])
     .pipe(beautify({indent_char: ' ', indent_size: 2}))
-    .pipe(trim())
+    .pipe(replace(/^\s*\r?\n/gm, ''))
     .pipe(gulp.dest('public'))
 });
 
-gulp.task('clean', ['html'], function () {
+gulp.task('clean', function () {
   return del([
-    'themes/hugo-moodlebox-theme/static/css/**/*.min.css',
-//     'themes/hugo-moodlebox-theme/static/js/**/*',
-    ''
+    'themes/hugo-moodlebox-theme/static/css/**/*.min.css'
   ]);
 });
+
+gulp.task('default', gulp.series( 'hugo', 'html', 'clean' ));
