a500ccf683ee1033b2c47f563060cac4405c293b
Nicolas Martignoni <nicolas@martignoni.net>
2022-07-01 08:08:06 +0200

    Remove unused JS code

--- assets/js/front.js
+++ assets/js/front.js
@@ -4,9 +4,7 @@ $(function () {
   sliderHomepage()
   sliders()
   fullScreenContainer()
-  productDetailGallery(4000)
   menuSliding()
-  productDetailSizes()
   utils()
 })
 
@@ -124,21 +122,6 @@ function pictureZoom () {
   })
 }
 
-/* full screen intro */
-function fullScreenContainer () {
-  var screenWidth = $(window).width() + 'px'
-  var screenHeight = '500px'
-
-  if ($(window).height() > 500) {
-    screenHeight = $(window).height() + 'px'
-  }
-
-  $('#intro, #intro .item').css({
-    width: screenWidth,
-    height: screenHeight
-  })
-}
-
 function utils () {
   /* tooltips */
   $('[data-toggle="tooltip"]').tooltip()
@@ -185,51 +168,6 @@ function utils () {
       scrollTop: targetTop
     }, 1000)
   }
-}
-
-/* product detail gallery */
-function productDetailGallery (confDetailSwitch) {
-  $('.thumb:first').addClass('active')
-  var timer = setInterval(autoSwitch, confDetailSwitch)
-
-  $('.thumb').click(function (e) {
-    switchImage($(this))
-    clearInterval(timer)
-    timer = setInterval(autoSwitch, confDetailSwitch)
-    e.preventDefault()
-  })
-
-  $('#mainImage').hover(function () {
-    clearInterval(timer)
-  }, function () {
-    timer = setInterval(autoSwitch, confDetailSwitch)
-  })
-
-  function autoSwitch () {
-    var nextThumb = $('.thumb.active').closest('div').next('div').find('.thumb')
-    if (nextThumb.length === 0) {
-      nextThumb = $('.thumb:first')
-    }
-    switchImage(nextThumb)
-  }
-
-  function switchImage (thumb) {
-    $('.thumb').removeClass('active')
-    var bigUrl = thumb.attr('href')
-    thumb.addClass('active')
-    $('#mainImage img').attr('src', bigUrl)
-  }
-}
-
-/* product detail sizes */
-function productDetailSizes () {
-  $('.sizes a').click(function (e) {
-    e.preventDefault()
-    $('.sizes a').removeClass('active')
-    $('.size-input').prop('checked', false)
-    $(this).addClass('active')
-    $(this).next('input').prop('checked', true)
-  })
 }
 
 $.fn.alignElementsSameHeight = function () {
