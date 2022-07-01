c08c13fa97b6f47ffcce63fb6cf47dea3fe385e2
Nicolas Martignoni <nicolas@martignoni.net>
2022-07-01 17:16:50 +0200

    Replace jQuery function with vanilla JS implementation

--- assets/js/front.js
+++ assets/js/front.js
@@ -110,47 +110,47 @@ function animationsSlider () {
   })
 }
 
-$.fn.alignElementsSameHeight = function () {
-  $('.same-height-row').each(function () {
-    var maxHeight = 0
-    var children = $(this).find('.same-height')
-    children.height('auto')
+/* Equal height */
+/* Stolen from https://dev.to/dhruvangg/equal-height-in-vanilla-javascript-49ch */
+function setHeight(el, val) {
+  if (typeof val === "function") val = val();
+  if (typeof val === "string") el.style.height = val;
+  else el.style.height = val + "px";
+}
 
-    if ($(window).width() > 768) {
-      children.each(function () {
-        if ($(this).innerHeight() > maxHeight) {
-          maxHeight = $(this).innerHeight()
-        }
-      })
-      children.innerHeight(maxHeight)
-    }
+var equalheight = function(container){
+  var currentTallest = 0,
+      currentRowStart = 0,
+      rowDivs = new Array(),
+      $el,
+      topPosition = 0;
 
-    maxHeight = 0
-    children = $(this).find('.same-height-always')
-    children.height('auto')
-    children.each(function () {
-      if ($(this).height() > maxHeight) {
-        maxHeight = $(this).innerHeight()
+  Array.from(document.querySelectorAll(container)).forEach((el,i) => {
+    el.style.height = "auto";
+    topPostion = el.offsetTop;
+    if(currentRowStart != topPostion){
+      for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+        setHeight(rowDivs[currentDiv], currentTallest)
       }
-    })
-    children.innerHeight(maxHeight)
+      rowDivs.length = 0;
+      currentRowStart = topPostion;
+      currentTallest = parseFloat(getComputedStyle(el, null).height.replace("px", ""))
+      rowDivs.push(el);
+    } else {
+      rowDivs.push(el);
+      currentTallest = (currentTallest < parseFloat(getComputedStyle(el, null).height.replace("px", ""))) ? (parseFloat(getComputedStyle(el, null).height.replace("px", ""))) : (currentTallest);
+    }
+    for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+      setHeight(rowDivs[currentDiv], currentTallest)
+    }
   })
 }
 
-var windowWidth
-$(function () {
-  windowWidth = $(window).width()
-
-  $(this).alignElementsSameHeight()
+window.addEventListener("load", function(){
+  equalheight('.same-height-always')
 })
-
-$(window).resize(function () {
-  var newWindowWidth = $(window).width()
-
-  if (windowWidth !== newWindowWidth) {
-    setTimeout(function () {
-      $(this).alignElementsSameHeight()
-    }, 205)
-    windowWidth = newWindowWidth
-  }
+window.addEventListener("resize", function(){
+  setTimeout(function(){
+    equalheight('.same-height-always')
+  })
 })
